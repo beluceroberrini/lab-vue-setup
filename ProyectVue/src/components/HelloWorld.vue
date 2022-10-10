512ad41e46e20c5937967622687e92f21c304a2c
Maria Belen <belul@hotmail.com>
2022-10-10 16:10:20 +0200

    projectVueLab

---
+++ ProyectVue/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 class="titulo">{{title}}</h1>
+  <div>
+  <button @click="helloWorld()" :class="clases">CLICK ME!</button>  
+  </div>
+
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+const helloWorld = () =>{
+  alert('Hello World')
+}
+const title = ref('Hello World')
+
+const clases = ['text', 'bg'];
+</script>
+
+<style scoped>
+
+.titulo{
+  background-color:yellowgreen;
+  font-weight: bold;
+  padding: 25px;
+
+}
+
+.text{
+  font-style: italic;
+  font-size: 25px;
+}
+.bg{
+  background-color: blueviolet;
+}
+</style>
